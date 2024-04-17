b7660d8171d02d57db8dfdcda075e7dc66b7294f
Vldslvtpchv15 <vladislavtopchiev@gmail.com>
2024-04-17 18:55:04 +0300

    Tank in progress

---
+++ CAR/CAR/main.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+using namespace std;
+#define MIN_TANK_VOLUME 20
+#define MAX_TANK_VOLUME 120
+
+class Tank
+{
+	const int VOLUME;
+	double fuel_level;
+public:
+	const int getVOLUME()const
+	{
+		return VOLUME;
+	}
+	double getfule_level()const
+	{
+		return fuel_level;
+	}
+	void fill(double ammount)
+	{
+		if (ammount < 0)
+		{
+			return;
+		}
+		if (fuel_level + ammount < VOLUME)
+		{
+			fuel_level += ammount;
+		}
+		else
+		{
+			fuel_level = VOLUME;
+		}
+	}
+	double give_fuel(double ammount)
+	{
+		if (ammount < 0)
+		{
+			return fuel_level;
+		}
+		if (fuel_level - ammount > 0)
+		{
+			return fuel_level - ammount;
+		}
+		else
+		{
+			fuel_level = 0;
+		}
+		return fuel_level;
+	}
+
+	Tank(int volume):VOLUME(volume)
+	{
+		/*if (volume < MIN_TANK_VOLUME) volume = MIN_TANK_VOLUME;
+		if (volume < MAX_TANK_VOLUME) volume = MAX_TANK_VOLUME;
+		this->VOLUME = volume;*/
+		this->fuel_level = 0;
+		cout << "Tank id ready" << this << endl;
+	}
+
+	void info() const
+	{
+		cout << "Tank volume: " << VOLUME << "liters.\n";
+		cout << "Fuel level: " << fuel_level << "liters.\n";
+	}
+};
+
+
+int main()
+{
+	setlocale(LC_ALL, "");
+	
+	Tank tank(50);
+	tank.info();
+}
